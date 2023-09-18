626f793d9aca5559ef97d4de169dc2588ad1201f
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-09-18 18:00:19 -0300

    simplify ddt

--- ddt/programs/ddt/src/lib.rs
+++ ddt/programs/ddt/src/lib.rs
@@ -6,30 +6,14 @@ declare_id!("4P1gf2p6cji6e23uw4FpKLqXx6ecg12rfBXnk8Mrrdyg");
 pub mod ddt {
     use super::*;
 
-    pub fn create_map(ctx: Context<CreateMap>, name: String, width: u64, length: u64, budget: u64) -> Result<()> {
+    pub fn create_map(ctx: Context<CreateMap>, name: String, board: [Element; 60], budget: u64) -> Result<()> {
         let map = &mut ctx.accounts.map;
         map.name = name;
-        map.elements = 0;
-        map.width = width;
-        map.length = length;
+        map.board = board.to_vec();
         map.authority = *ctx.accounts.user.key;
         map.budget = budget;
         map.bump = *ctx.bumps.get("map").unwrap();
 
-        Ok(())
-    }
-
-    pub fn add_element_to_map(ctx: Context<AddElementToMap>, kind: String, health: u64, dps: u64, gaia: bool, position_x: u64, position_y: u64, index: u64) -> Result<()> {
-        let map = &mut ctx.accounts.map;
-        let element = &mut ctx.accounts.element;
-        element.kind = kind;
-        element.health = health;
-        element.dps = dps;
-        element.gaia = gaia;
-        element.position_x = position_x;
-        element.position_y = position_y;
-        element.bump = *ctx.bumps.get("element").unwrap();
-        map.elements += 1;
         Ok(())
     }
 
@@ -40,23 +24,98 @@ pub mod ddt {
         game.started_at = Clock::get()?.slot.checked_add(100).unwrap();
         game.status = "created".to_string();
         game.points = 0;
-        game.elements = map.elements;
+        for i in 0..60 {
+            game.map[i] = Element {
+                kind: map.board[i].kind.to_string(),
+                health: map.board[i].health,
+                dps: map.board[i].dps,
+                gaia: true,
+            };
+        }
+
         game.budget = map.budget;
         game.bump = *ctx.bumps.get("game").unwrap();
         Ok(())
     }
 
-    pub fn add_element_to_game(ctx: Context<AddElementToGame>, kind: String, health: u64, dps: u64, gaia: bool, position_x: u64, position_y: u64, index: u64) -> Result<()> {
-        let game = &mut ctx.accounts.game;
-        let element = &mut ctx.accounts.element;
-        element.kind = kind;
-        element.health = health;
-        element.dps = dps;
-        element.gaia = gaia;
-        element.position_x = position_x;
-        element.position_y = position_y;
-        element.bump = *ctx.bumps.get("element").unwrap();
-        game.elements += 1;
+    pub fn add_element_to_game(ctx: Context<PlayGame>, deploys: Vec<Deploy>) -> Result<()> {
+        let game = &mut *ctx.accounts.game;
+        let units= [
+            Unit {
+                kind: "soldier".to_string(),
+                health: 100,
+                dps: 10,
+                cost: 10,
+            },
+            Unit {
+                kind: "tank".to_string(),
+                health: 200,
+                dps: 25,
+                cost: 20,
+
+            },
+            Unit {
+                kind: "plane".to_string(),
+                health: 50,
+                dps: 75,
+                cost: 50,
+            }
+        ];
+        let mut board = game.map.iter().map(|e| e.clone()).collect::<Vec<Element>>();
+        let mut budget = game.budget;
+        let mut points = game.points;
+        for deploy in deploys {
+            let unit = units.iter().find(|u| u.kind == deploy.kind).unwrap();
+            if budget >= unit.cost {
+                board[deploy.position as usize] = Element {
+                    kind: unit.kind.to_string(),
+                    health: unit.health,
+                    dps: unit.dps,
+                    gaia: false,
+                };
+                budget -= unit.cost;
+            }else{
+                return Err(ErrorCode::CostExceedsBudget.into());
+            }
+        }
+
+        for i in 0..6 {
+            let mut j = 9;
+            while j > 0 {
+                if board[i*10+j].gaia == false {
+                    board[i*10+j-1].health -= board[i*10+j].dps;
+                    board[i*10+j].health -= board[i*10+j-1].dps;
+                    points += board[i*10+j].dps;
+                    if board[i*10+j-1].health <= 0 {
+                        board[i*10+j-1] = Element {
+                            kind: "".to_string(),
+                            health: 0,
+                            dps: 0,
+                            gaia: true,
+                        };
+                    }
+                }
+                j -= 1;
+            }
+        }
+        if board[0].health <= 0 {
+            points += 100;
+            game.status = "win".to_string();
+        } else {
+            game.status = "lose".to_string();
+        }
+        game.budget = budget;
+        game.points = points;
+
+        for i in 0..60 {
+            game.map[i] = Element {
+                kind: board[i].kind.to_string(),
+                health: board[i].health,
+                dps: board[i].dps,
+                gaia: board[i].gaia,
+            };
+        }
+
         Ok(())
     }
 
@@ -68,7 +127,7 @@ pub struct CreateMap<'info> {
     #[account(
         init,
         payer = user,
-        space = 8 + 8 + 8 + 8 + 32 + 1,
+        space = 1000,
         seeds = [name.as_ref()],
         bump
     )]
@@ -78,22 +137,6 @@ pub struct CreateMap<'info> {
     pub system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-#[instruction(index: u64)]
-pub struct AddElementToMap<'info> {
-    pub map: Account<'info, Map>,
-    #[account(
-        init,
-        payer = user,
-        space = 100,
-        seeds = [&index.to_le_bytes()],
-        bump
-    )]
-    pub element: Account<'info, Element>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
 
 #[derive(Accounts)]
 pub struct CreateGame<'info> {
@@ -109,29 +152,21 @@ pub struct CreateGame<'info> {
     pub system_program: Program<'info, System>,
 }
 
-
 #[derive(Accounts)]
-#[instruction(index: u64)]
-pub struct AddElementToGame<'info> {
+pub struct PlayGame<'info> {
+    #[account(mut)]
     pub game: Account<'info, Game>,
-    #[account(
-        init,
-        payer = user,
-        space = 100,
-        seeds = [&index.to_le_bytes()],
-        bump
-    )]
-    pub element: Account<'info, Element>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
+
 #[account]
 pub struct Game {
     pub player: Pubkey,
     pub started_at: u64,
-    pub elements: u64,
+    pub map: Vec<Element>,
     pub points: u64,
     pub status: String,
     pub budget: u64,
@@ -141,31 +176,37 @@ pub struct Game {
 #[account]
 pub struct Map {
     pub name: String,
-    pub elements: u64,
-    pub width: u64,
-    pub length: u64,
-    pub authority: Pubkey,
+    pub board: Vec<Element>,
     pub budget: u64,
-    pub bump: u8,
-}
-
-#[account]
-pub struct Element {
-    pub kind: String,
-    pub health: u64,
-    pub dps: u64,
-    pub gaia: bool, 
-    pub position_x: u64,
-    pub position_y: u64,
+    pub authority: Pubkey,
     pub bump: u8,
 }
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct NewElement {
+pub struct Element {
     pub kind: String,
     pub health: u64,
     pub dps: u64,
-    pub gaia: bool, 
-    pub position_x: u64,
-    pub position_y: u64,
+    pub gaia: bool,
+}
+
+#[account]
+pub struct Unit {
+    pub kind: String,
+    pub health: u64,
+    pub dps: u64,
+    pub cost: u64,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Deploy {
+    pub kind: String,
+    pub position: u8,
+}
+
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Cost exceeds budget")]
+    CostExceedsBudget,
 }
