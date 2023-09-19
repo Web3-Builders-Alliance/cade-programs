6afab17bb670c80f14c8e81edbad8c3ed229a3f3
Ahzam Akhtar <ahzam8186@gmail.com>
2023-09-19 23:04:40 +0530

    Ticket Program Upgraded

--- ticket-program/programs/ticket-program/src/lib.rs
+++ ticket-program/programs/ticket-program/src/lib.rs
@@ -1,81 +1,166 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token;
-use anchor_spl::token::{Token, MintTo, Transfer};
+use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::program::invoke_signed};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+};
+use anchor_spl::token::{self, Transfer as SplTransfer};
+use solana_program::system_instruction;
+use mpl_token_metadata::instruction::create_metadata_accounts_v3;
 
-
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FiWn9Mm9PQvGRS3Rw48kigPTzXnmcm5v3NZrWFVZJnFp");
 
 #[program]
-pub mod token_contract {
+mod cade_token_minter {
     use super::*;
+    pub fn init_cade(ctx: Context<Initialize>, metadata: InitTokenParams) -> Result<()> {
+        let seeds = &["mint".as_bytes(), &[*ctx.bumps.get("mint").unwrap()]];
+        let signer = [&seeds[..]];
 
-    pub fn mint_token(
-        ctx: Context<MintToken>,
-        amount : u64
-        ) -> Result<()> {
-        // Create the MintTo struct for our context
-        let cpi_accounts = MintTo {
-            mint: ctx.accounts.mint.to_account_info(),
-            to: ctx.accounts.token_account.to_account_info(),
-            authority: ctx.accounts.authority.to_account_info(),
-        };
-        
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        // Create the CpiContext we need for the request
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        let account_info = vec![
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
 
-        // Execute anchor's helper function to mint tokens
-        token::mint_to(cpi_ctx, amount)?;
-        
+        invoke_signed(
+            &create_metadata_accounts_v3(
+                ctx.accounts.token_metadata_program.key(), // token metadata program
+                ctx.accounts.metadata.key(),               // metadata account PDA for mint
+                ctx.accounts.mint.key(),                   // mint account
+                ctx.accounts.mint.key(),                   // mint authority
+                ctx.accounts.payer.key(),                  // payer for transaction
+                ctx.accounts.mint.key(),                   // update authority
+                metadata.name,                             // name
+                metadata.symbol,                           // symbol
+                metadata.uri,                              // uri (offchain metadata)
+                None,                                      // (optional) creators
+                0,                                         // seller free basis points
+                true,                                      // (bool) update authority is signer
+                true,                                      // (bool) is mutable
+                None,                                      // (optional) collection
+                None,                                      // (optional) uses
+                None,                                      // (optional) collection details
+            ),
+            account_info.as_slice(),
+            &signer,
+        )?;
+
+        msg!("Token mint created successfully.");
+
         Ok(())
     }
 
-    pub fn transfer_token(
-        ctx: Context<TransferToken>,
-        amount : u64
-        ) -> Result<()> {
-        // Create the Transfer struct for our context
-        let transfer_instruction = Transfer{
-            from: ctx.accounts.from.to_account_info(),
-            to: ctx.accounts.to.to_account_info(),
-            authority: ctx.accounts.from_authority.to_account_info(),
-        };
-         
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        // Create the Context for our Transfer request
-        let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
+    pub fn mint_cade(ctx: Context<MintTokens>, quantity: u64) -> Result<()> {
+        let seeds = &["mint".as_bytes(), &[*ctx.bumps.get("mint").unwrap()]];
+        let signer = [&seeds[..]];
 
-        // Execute anchor's helper function to transfer tokens
-        anchor_spl::token::transfer(cpi_ctx, amount)?;
- 
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+                &signer,
+            ),
+            quantity,
+        )?;
+
         Ok(())
     }
 
+    pub fn transfer_cade_tokens(ctx: Context<TransferSpl>, amount: u64) -> Result<()> {
+        let destination = &ctx.accounts.to_ata;
+        let source = &ctx.accounts.from_ata;
+        let token_program = &ctx.accounts.token_program;
+        let authority = &ctx.accounts.from;
+
+        // Transfer tokens from taker to initializer
+        let cpi_accounts = SplTransfer {
+            from: source.to_account_info().clone(),
+            to: destination.to_account_info().clone(),
+            authority: authority.to_account_info().clone(),
+        };
+        let cpi_program = token_program.to_account_info();
+        
+        token::transfer(
+            CpiContext::new(cpi_program, cpi_accounts),
+            amount)?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct MintToken<'info> {
-    /// CHECK: This is the token that we want to mint
+#[instruction(
+    params: InitTokenParams
+)]
+pub struct Initialize<'info> {
+    /// CHECK: New Metaplex Account being created
     #[account(mut)]
-    pub mint: UncheckedAccount<'info>,
+    pub metadata: UncheckedAccount<'info>,
+    #[account(
+        init,
+        seeds = [b"mint"],
+        bump,
+        payer = payer,
+        mint::decimals = params.decimals,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
-    /// CHECK: This is the token account that we want to mint tokens to
-    #[account(mut)]
-    pub token_account: UncheckedAccount<'info>,
-    /// CHECK: the authority of the mint account
-    #[account(mut)]
-    pub authority: AccountInfo<'info>,
+    /// CHECK: Metaplex program ID
+    pub token_metadata_program: UncheckedAccount<'info>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Debug, Clone)]
+pub struct InitTokenParams {
+    pub name: String,
+    pub symbol: String,
+    pub uri: String,
+    pub decimals: u8,
 }
 
 #[derive(Accounts)]
-pub struct TransferToken<'info> {
+pub struct MintTokens<'info> {
+    #[account(
+        mut,
+        seeds = [b"mint"],
+        bump,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = payer,
+    )]
+    pub destination: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
-    /// CHECK: The associated token account that we are transferring the token from
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[derive(Accounts)]
+pub struct TransferSpl<'info> {
+    pub from: Signer<'info>,
     #[account(mut)]
-    pub from: UncheckedAccount<'info>,
-    /// CHECK: The associated token account that we are transferring the token to
+    pub from_ata: Account<'info, TokenAccount>,
     #[account(mut)]
-    pub to: AccountInfo<'info>,
-    // the authority of the from account 
-    pub from_authority: Signer<'info>,
+    pub to_ata: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
 }
--- ticket-program/tests/ticket-program.ts
+++ ticket-program/tests/ticket-program.ts
@@ -4,13 +4,134 @@ import { TicketProgram } from "../target/types/ticket_program";
 
 describe("ticket-program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const METADATA_SEED = "metadata";
+    const TOKEN_METADATA_PROGRAM_ID = new web3.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
 
-  const program = anchor.workspace.TicketProgram as Program<TicketProgram>;
+    // Constants from our program
+    const MINT_SEED = "mint";
+  
+    // Data for our tests
+    const payer = pg.wallet.publicKey;
+    const metadata = {
+      name: "Cade",
+      symbol: "CDX",
+      uri: "https://pan5exjah3p3wqjeujj4kylo4vgt5znvwrt77lnf5gcmm6tadsea.arweave.net/eBvSXSA-37tBJKJTxWFu5U0-5bW0Z_-tpemExnpgHIg",
+      decimals: 9,
+    };
+    const mintAmount = 10;
+    const [mint] = web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(MINT_SEED)],
+      pg.PROGRAM_ID
+    );
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    const [metadataAddress] = web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from(METADATA_SEED),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mint.toBuffer(),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    );
+
+    // Test init token
+    it("init cade", async () => {
+      const info = await pg.connection.getAccountInfo(mint);
+      if (info) {
+        return; // Do not attempt to initialize if already initialized
+      }
+      console.log("  Mint not found. Attempting to initialize.");
+   
+      const context = {
+        metadata: metadataAddress,
+        mint,
+        payer,
+        rent: web3.SYSVAR_RENT_PUBKEY,
+        systemProgram: web3.SystemProgram.programId,
+        tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+        tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+      };
+  
+      const txHash = await pg.program.methods
+        .initCade(metadata)
+        .accounts(context)
+        .rpc();
+  
+      await pg.connection.confirmTransaction(txHash, 'finalized');
+      console.log(`  https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+      const newInfo = await pg.connection.getAccountInfo(mint);
+      assert(newInfo, "  Mint should be initialized.");
+    });
+
+     // Test mint tokens
+     it("mint cade", async () => {
+      const destination = await anchor.utils.token.associatedAddress({
+        mint: mint,
+        owner: payer,
+      });
+  
+      let initialBalance: number;
+      try {
+        const balance = (await pg.connection.getTokenAccountBalance(destination))
+        initialBalance = balance.value.uiAmount;
+      } catch {
+        // Token account not yet initiated has 0 balance
+        initialBalance = 0;
+      } 
+      
+      const context = {
+        mint,
+        destination,
+        payer,
+        rent: web3.SYSVAR_RENT_PUBKEY,
+        systemProgram: web3.SystemProgram.programId,
+        tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+        associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
+      };
+  
+      const txHash = await pg.program.methods
+        .mintCade(new BN(mintAmount * 10 ** metadata.decimals))
+        .accounts(context)
+        .rpc();
+      await pg.connection.confirmTransaction(txHash);
+      console.log(`  https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+  
+     
+      });
+
+      it("transfer", async () => {
+        const fromKp = pg.wallet.keypair;
+        const to = new PublicKey("2JSg1MdNqRg9z4RP7yiE2NV86fux2BNtF3pSDjhoi767s")
+    
+        const mint = new PublicKey("CHwKjh5GbbtDJDqnGKLwv1nZTJKuGAqhZkpjcwxqkyz3")
+        
+        const fromAta = await createAssociatedTokenAccount(
+          pg.program.provider.connection,
+          pg.wallet.keypair,
+          mint,
+          fromKp.publicKey
+        );
+    
+         const toAta = await createAssociatedTokenAccount(
+          pg.program.provider.connection,
+          pg.wallet.keypair,
+          mint,
+          to
+        );
+    
+         const transferAmount = new BN(2);
+    
+         const txHash = await pg.program.methods
+          .transferSplTokens(transferAmount)
+          .accounts({
+            from: fromKp.publicKey,
+            fromAta: fromAta,
+            toAta: toAta,
+            tokenProgram: TOKEN_PROGRAM_ID,
+          })
+          .signers([pg.wallet.keypair, fromKp])
+          .rpc();
+    
+          console.log(`https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+          await pg.connection.confirmTransaction(txHash, "finalized");
+      });
 });
