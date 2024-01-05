e10f87f00ac0caabd0ed0845ba8b3e650950e184
Marche <juanpatriciomarchetto@gmail.com>
2024-01-05 17:47:44 -0300

    Simplify 4-in-line (#9)

--- 4-in-line/tests/four-in-line.ts
+++ 4-in-line/tests/four-in-line.ts
@@ -1,27 +1,21 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { FourInLine } from "../target/types/four_in_line";
-import { PublicKey, SystemProgram } from "@solana/web3.js"
+import { PublicKey, Keypair } from "@solana/web3.js"
 import * as web3 from "@solana/web3.js"
 
 describe("four-in-line", () => {
-  const name = "Test Game"
-
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.FourInLine as Program<FourInLine>;
   const payer = (program.provider as anchor.AnchorProvider).wallet;
   const player2 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
-
-  const [pdaPublicKey] = web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("game"), Buffer.from(name)],
-    program.programId,
-  )
+  const game = new Keypair()
+  
 
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.createGame(
-      name,
       [
         payer.publicKey,
         player2
@@ -29,9 +23,15 @@ describe("four-in-line", () => {
       ""
     )
       .accounts({
-        payer: payer.publicKey
+        payer: payer.publicKey,
+        game: game.publicKey,
       })
-      .rpc();
+      .signers([game])
+      .rpc({
+        skipPreflight: true,
+      }).catch((err) => {
+        console.log(err)
+        });
     console.log("Your transaction signature", tx);
     const games = await program.account.game.all()
     console.log(games)
